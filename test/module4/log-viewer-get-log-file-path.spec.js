965dae3d7039996500e8a73d3a7296dbcb1202d2
said.ouarrich <said.ouarrich-ext@socgen.com>
2020-07-03 15:25:39 +0200

    module4

--- test/module4/log-viewer-get-log-file-path.spec.js
+++ test/module4/log-viewer-get-log-file-path.spec.js
@@ -7,12 +7,14 @@ describe('log-viewer.js', () => {
     const logViewer = jscs(source);
     
     const logFilePath = logViewer.findVariable('filePath');
+
     const logFilePathMatch = {
       'init.object.callee.object.name': 'document',
       'init.object.callee.property.name': 'getElementById',
       'init.object.arguments.0.value': 'logFilePath',
       'init.property.name': "value"
     };
+    
     assert(matchObj(logFilePath, logFilePathMatch), 
       'Are you getting a reference to the logFilePath input field value using `document.getElementById()` to query?');
   });
